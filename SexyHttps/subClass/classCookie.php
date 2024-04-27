f328d66e58c0c6aaff3faa4dbc8436ef65e7b5f7
Cristiansajksa <155487691+Cristiansajksa@users.noreply.github.com>
2024-04-26 20:46:23 -0400

    Update classCookie.php

--- SexyHttps/subClass/classCookie.php
+++ SexyHttps/subClass/classCookie.php
@@ -3,9 +3,7 @@ class CookieRequest
 {
     public function ParseCookie(string $resultHttp) : void
     {
-        if ( 
-            !empty( preg_match_all("#(?<=set-cookie: )\S{3,}(?= )#i", $resultHttp, $matchCookie) )
-        ) {
+        if (empty(preg_match_all("#(?<=set-cookie: )\S{3,}(?= )#i", $resultHttp, $matchCookie))) {
             $cookie = join( " ", $matchCookie[0] );
             sexyHttps::$cookieSession[sexyHttps::$url] = empty( sexyHttps::$cookieSession[sexyHttps::$url] ) ?
             $cookie : self::verifyAtributesCookie( $matchCookie[0] );
@@ -16,7 +14,7 @@ class CookieRequest
 
     public function UsedCookie(string $url) : void
     {
-        if (isset( sexyHttps::$cookieSession[sexyHttps::$url] )) {
+        if (isset(sexyHttps::$cookieSession[sexyHttps::$url])) {
             curl_setopt(
                 sexyHttps::$objectCurl,
                 CURLOPT_COOKIE,
@@ -36,7 +34,6 @@ class CookieRequest
             str_replace("=", "", strstr($attributeCookie, "=", false));
         }
     
-    
         return $formatArrayCookie;
     }
 
@@ -47,7 +44,7 @@ class CookieRequest
         $keepAttCookie = self::FormatArrayCookie( $attributesCookie );
         $cookieCopy = sexyHttps::$cookieSession[sexyHttps::$url];
         foreach ($keepAttCookie as $attCookie => $valueCookie) {
-            if (strstr( $cookieCopy, $attCookie )) {
+            if (strstr($cookieCopy, $attCookie)) {
 
                 $cookieCopy = preg_replace( 
                     "#(?<=$attCookie=)\S+#", 
