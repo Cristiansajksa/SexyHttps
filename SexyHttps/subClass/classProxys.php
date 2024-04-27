7fc85f49498fa05d7850999f6c9f949230a15ec1
Cristiansajksa <155487691+Cristiansajksa@users.noreply.github.com>
2024-04-26 20:46:52 -0400

    Update classProxys.php

--- SexyHttps/subClass/classProxys.php
+++ SexyHttps/subClass/classProxys.php
@@ -5,12 +5,12 @@ class ProxysRequest
     {
         for (
             $countProxyCheck = 0; 
-            ($countProxyCheck <= 7 and !self::ProxyChecker( $serverProxyInfo )); 
+            ($countProxyCheck <= 7 and !self::ProxyChecker($serverProxyInfo)); 
             $countProxyCheck++
         );
 
         if ($countProxyCheck === 8) {
-            throw new exception("Proxys DD");
+            throw new exception( "Proxys DD" );
         }
         return true;
     }
@@ -21,16 +21,16 @@ class ProxysRequest
     {
         self::VerifyConstValueArray( $serverProxyInfo );
         sexyHttps::$keepProxys = $serverProxyInfo;
-        if (isset( $serverProxyInfo )) {
+        if (isset($serverProxyInfo)) {
             $ch = curl_init( "http://ip-api.com/json" );
-            curl_setopt_array( $ch, $serverProxyInfo );
 
+            curl_setopt_array( $ch, $serverProxyInfo );
             curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
             $resultCurl = json_decode( curl_exec($ch) );
             curl_close( $ch );
 
             
-            if (empty( $resultCurl->query )) {
+            if (empty($resultCurl->query)) {
                 return false;
             } else {
                 curl_setopt_array( sexyHttps::$objectCurl, $serverProxyInfo );
@@ -45,14 +45,13 @@ class ProxysRequest
     {
         foreach ($arrayInfo as $key => $value) {
             unset( $arrayInfo[$key] );
-            if (!defined( $key )) {
+            if (!defined($key)) {
                 continue;
             }
 
             $value = is_file( $value ) ?
             file( $value, FILE_IGNORE_NEW_LINES )[array_rand(file($value, FILE_IGNORE_NEW_LINES))] :
             $value;
-
             $arrayInfo[constant($key)] = $value;
         }
     }
