21677c26946f7eaa81b9ec635fbeff251cb93094
Cristiansajksa <155487691+Cristiansajksa@users.noreply.github.com>
2024-08-10 06:42:39 -0400

    Update traitRetrys.php

--- SexyHttps/trait/traitRetrys.php
+++ SexyHttps/trait/traitRetrys.php
@@ -24,7 +24,51 @@ trait TraitRetrysRequest
 
         return empty($primaryIp) || self::searchHtml($resp, $searchCoin) || $resp == $msgExecute;
     }
+<?php
+trait TraitRetrysRequest
+{
+    private static function executeRetrys(string $msgExecute, string|array $searchCoin) : string
+    {
+        for (
+            $countRetrys = 0;
+            $response = self::retry($msgExecute, $searchCoin) and $countRetrys < 10;
+            $countRetrys++
+        );
 
+        if ($countRetrys == 10) {
+            throw new exception( "retry exceeded! (10)" );
+        }
+        return $response;
+    }
+
+
+
+    private static function retry(?string $msgExecute, string|array $searchCoin) : string
+    {
+        !self::$basicConfig["NewCurlRetry"] ?: self::$objectOthor->NewObjectCurl();
+        $resp = curl_exec( self::$objectCurl );
+        $primaryIp = curl_getinfo( self::$objectCurl )["primary_ip"];
+
+        curl_close( sexyHttps::$objectCurl );
+        return empty($primaryIp) || self::searchHtml($resp, $searchCoin) || $resp == $msgExecute;
+    }
+
+
+
+    private static function searchHtml(string $html, string|array $searchCoin) : bool 
+    {
+        $searchCoin = (array) $searchCoin;
+
+        foreach ($searchCoina as $stringCoin) {
+            $resultSearch = stristr( $html, $searchCoin );
+            if ($resultSearch) {
+                break;
+            }
+        }
+
+        return $resultSearch;
+    }
+}
 
 
     private static function searchHtml(string $html, string|array $searchCoin) : bool 
