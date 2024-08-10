1064de0600b222869260df0560e6dbfa0b4de81b
Cristiansajksa <155487691+Cristiansajksa@users.noreply.github.com>
2024-08-10 06:15:49 -0400

    Update classProxys.php

--- SexyHttps/subClass/classProxys.php
+++ SexyHttps/subClass/classProxys.php
@@ -1,44 +1,11 @@
 <?php
 class ProxysRequest
 {
-    public static function UsedProxys(array $serverProxyInfo) : void
-    {
-        for (
-            $countProxyCheck = 0; 
-            ($countProxyCheck <= 7 and !self::ProxyChecker($serverProxyInfo)); 
-            $countProxyCheck++
-        );
-
-        if ($countProxyCheck === 8) {
-            throw new exception( "An error has occurred in the check-in process" );
-        }
-    }
-
-
-    
-    private static function ProxyChecker(array $serverProxyInfo) : bool 
+    public static function keepProxys(array $serverProxyInfo) : void
     {
         self::VerifyConstValueArray( $serverProxyInfo );
         sexyHttps::$keepConfig += $serverProxyInfo;
-
-        if (isset($serverProxyInfo)) {
-            $ch = curl_init( "http://ip-api.com/json" );
-            curl_setopt_array( $ch, $serverProxyInfo );
-            curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-
-            curl_setopt( $ch, CURLOPT_TIMEOUT, 15 );
-            $resultCurl = json_decode( curl_exec($ch) );
-            curl_close( $ch );
-
-            if (empty($resultCurl->query)) {
-                return false;
-            } else {
-                curl_setopt_array( sexyHttps::$objectCurl, $serverProxyInfo );
-                return true;
-            }
-        }
     }
-
 
 
     public static function VerifyConstValueArray(array &$arrayInfo) : void
